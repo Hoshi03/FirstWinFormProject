1fd934707591aa9f5a4bffe314e50f9cf3a2ae64
Hoshi03 <ichanho114@gmail.com>
2024-09-20 19:55:00 +0900

    초기 커밋.

---
+++ FirstWinFormProject/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstWinFormProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("hi");
+            lb1.Text = "hello world!";
+            button1.SetBounds(0, 0, 100, 100);
+            button1.Click += MyButton_Click;
+
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            label3.Text = "btn1 clicked!";
+        }
+
+        private void MyButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button clicked!");
+        }
+    }
+}
